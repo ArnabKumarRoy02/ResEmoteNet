5130a8a7c7e95ccb44dced638170b96cdabf466c
Arnab Kumar Roy <arnabroy770@gmail.com>
2024-04-02 16:05:31 +0530

    📂 vgg training file

---
+++ vgg_train.py
@@ -0,0 +1,163 @@
+import torch
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+from approach.vgg import VGG
+from get_dataset import Four4All
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print(f"Using {device} device")
+
+# Transform the dataset
+transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+# Load the dataset
+fer_dataset_train = Four4All(csv_file='data/train_labels.csv',
+                             img_dir='data/train', transform=transform)
+
+data_train_loader = DataLoader(fer_dataset_train, batch_size=16, shuffle=True)
+train_image, train_label = next(iter(data_train_loader))
+
+val_dataset = Four4All(csv_file='data/valid_labels.csv',
+                       img_dir='data/valid/', transform=transform)
+
+data_val_loader = DataLoader(val_dataset, batch_size=16, shuffle=True)
+val_image, val_label = next(iter(data_val_loader))
+
+fer_dataset_test = Four4All(csv_file='data/test_labels.csv',
+                            img_dir='data/test', transform=transform)
+
+data_test_loader = DataLoader(fer_dataset_test, batch_size=16, shuffle=False)
+test_image, test_label = next(iter(data_test_loader))
+
+print(f"Train batch: Image shape {train_image.shape}, Label shape {train_label.shape}")
+print(f"Validation batch: Image shape {val_image.shape}, Label shape {val_label.shape}")
+print(f"Test batch: Image shape {test_image.shape}, Label shape {test_label.shape}")
+
+# Load the model
+model = VGG().to(device)
+
+# Print the number of parameters
+total_params = sum(p.numel() for p in model.parameters())
+print(f'{total_params:,} total parameters.')
+
+# Hyperparameters
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
+
+patience = 15
+best_val_acc = 0
+patience_counter = 0
+
+num_epochs = 80
+
+train_losses = []
+val_losses = []
+train_accuracies = []
+val_accuracies = []
+test_losses = []
+test_accuracies = []
+
+# Start training
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for data in tqdm(data_train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    train_loss = running_loss / len(data_train_loader)
+    train_acc = correct / total
+    train_losses.append(train_loss)
+    train_accuracies.append(train_acc)
+
+    model.eval()
+    test_running_loss = 0.0
+    test_correct = 0
+    test_total = 0
+    with torch.no_grad():
+        for data in data_test_loader:
+            inputs, labels = data[0].to(device), data[1].to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            test_running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            test_total += labels.size(0)
+            test_correct += (predicted == labels).sum().item()
+
+    test_loss = test_running_loss / len(data_test_loader)
+    test_acc = test_correct / test_total
+    test_losses.append(test_loss)
+    test_accuracies.append(test_acc)
+
+    model.eval()
+    val_running_loss = 0.0
+    val_correct = 0
+    val_total = 0
+    with torch.no_grad():
+        for data in data_val_loader:
+            inputs, labels = data[0].to(device), data[1].to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            val_total += labels.size(0)
+            val_correct += (predicted == labels).sum().item()
+
+    val_loss = val_running_loss / len(data_val_loader)
+    val_acc = val_correct / val_total
+    val_losses.append(val_loss)
+    val_accuracies.append(val_acc)
+
+    print(f"Epoch {epoch+1}, Train Loss: {train_loss}, Train Accuracy: {train_acc}, Test Loss: {test_loss}, Test Accuracy: {test_acc}, Validation Loss: {val_loss}, Validation Accuracy: {val_acc}")
+    
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        patience_counter = 0 
+        torch.save(model.state_dict(), 'best_model.pth')
+    else:
+        patience_counter += 1
+        print(f"No improvement in validation accuracy for {patience_counter} epochs.")
+    
+    if patience_counter > patience:
+        print("Stopping early due to lack of improvement in validation accuracy.")
+        break
+
+df = pd.DataFrame({
+    'Epoch': range(1, 80), # change this number after '(1, _)' to num_epochs+1
+    'Train Loss': train_losses,
+    'Test Loss': test_losses,
+    'Validation Loss': val_losses,
+    'Train Accuracy': train_accuracies,
+    'Test Accuracy': test_accuracies,
+    'Validation Accuracy': val_accuracies
+})
+df.to_csv('result_gimefive.csv', index=False)
