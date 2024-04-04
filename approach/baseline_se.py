043ece8abe968dc608adbdbe8044fc87107f09b6
Arnab Kumar Roy <arnabroy770@gmail.com>
2024-04-04 19:15:35 +0530

    📦 baseline + SE

---
+++ approach/baseline_se.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction, in_channels, bias=False),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class BaselineModel(nn.Module):
+    def __init__(self):
+        super(BaselineModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(1024)
+        self.se = SEBlock(1024)
+        
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc1 = nn.Linear(1024, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.droupout1 = nn.Dropout(0.2)
+        self.droupout2 = nn.Dropout(0.5)
+        self.fc4 = nn.Linear(512, 6)
+        
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+        x = self.droupout1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+        x = self.droupout1(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.max_pool2d(x, 2)
+        x = self.droupout1(x)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.max_pool2d(x, 2)
+        x = self.droupout1(x)
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.max_pool2d(x, 2)
+        x = self.se(x)
+        # x = self.droupout1(x)
+        
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.droupout2(x)
+        x = F.relu(self.fc2(x))
+        x = self.droupout2(x)
+        x = F.relu(self.fc3(x))
+        x = self.droupout2(x)
+        x = self.fc4(x)
+        return x
