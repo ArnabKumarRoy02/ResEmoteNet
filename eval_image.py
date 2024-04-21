c3d0353c78c60f46a7684578ca0ce476743ca2ad
Arnab Kumar Roy <arnabroy770@gmail.com>
2024-04-21 10:27:50 +0530

    ✅ script for image testing added

---
+++ eval_image.py
@@ -0,0 +1,92 @@
+import cv2
+import cv2.data
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+from approach.fourforall import FourforAll
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+# Emotions labels
+emotions = ['happiness', 'surprise', 'sadness', 'anger', 'fear', 'disgust']
+
+model = FourforAll().to(device)
+model.load_state_dict(torch.load('best_four4all.pth', map_location=device))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+)
+
+# Settings for text
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1.2
+font_color = (0, 255, 0)  # This is BGR color
+thickness = 3
+line_type = cv2.LINE_AA
+
+max_emotion = ''
+
+def detect_emotion(image):
+    img_tensor = transform(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        outputs = model(img_tensor)
+        probabilities = F.softmax(outputs, dim=1)
+    scores = probabilities.cpu().numpy().flatten()
+    rounded_scores = [round(score, 2) for score in scores]
+    return rounded_scores
+
+def get_max_emotion(x, y, w, h, image):
+    crop_img = image[y : y + h, x : x + w]
+    pil_crop_img = Image.fromarray(crop_img)
+    rounded_scores = detect_emotion(pil_crop_img)    
+    max_index = np.argmax(rounded_scores)
+    max_emotion = emotions[max_index]
+    return max_emotion
+
+def print_max_emotion(x, y, image, max_emotion):
+    org = (x, y - 15)
+    cv2.putText(image, max_emotion, org, font, font_scale, font_color, thickness, line_type)
+    
+def print_all_emotion(x, y, w, h, image):
+    crop_img = image[y : y + h, x : x + w]
+    pil_crop_img = Image.fromarray(crop_img)
+    rounded_scores = detect_emotion(pil_crop_img)
+    org = (x + w + 10, y - 20)
+    for index, value in enumerate(emotions):
+        emotion_str = (f'{value}: {rounded_scores[index]:.2f}')
+        y = org[1] + 40
+        org = (org[0], y)
+        cv2.putText(image, emotion_str, org, font, font_scale, font_color, thickness, line_type)
+    
+def detect_bounding_box(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        # Draw bounding box on face
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        max_emotion = get_max_emotion(x, y, w, h, image)
+        print_max_emotion(x, y, image, max_emotion)
+        print_all_emotion(x, y, w, h, image)
+    
+    return faces
+
+# Load the image file
+image = cv2.imread('image_test.jpg')
+# Process the image
+faces = detect_bounding_box(image)
+
+# Display the processed image
+cv2.imshow("Four4All Image Test", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
