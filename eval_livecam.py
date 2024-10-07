7dfd404d487c07980eedfed397d7c8975ced5702
Arnab Kumar Roy <arnabroy770@gmail.com>
2024-10-07 13:13:21 +0530

    ✅ inference with loaded checkpoint

--- eval_livecam.py
+++ eval_livecam.py
@@ -14,7 +14,8 @@ device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 emotions = ['happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'neutral']
 
 model = ResEmoteNet().to(device)
-model.load_state_dict(torch.load('best_model.pth', map_location=device))
+checkpoint = torch.load('best_model.pth', weights_only=True)
+model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
 
